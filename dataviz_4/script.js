0c2e0b03022c3e1fee10f4989cc65db31690dbf5
milirodriguezv <milagrosrodriguez1602@gmail.com>
2023-03-15 15:43:56 -0300

    viz4

--- dataviz_4/script.js
+++ dataviz_4/script.js
@@ -8,13 +8,38 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
         {x: 'anio_mision', y: 'mision_hs',
         fill: d => (d.anio_mision == 2016 ? 'purple' :  'orange')},
       )),
+      // Plot.text(data, {
+      //   x: 'anio_mision',
+      //   y: 'mision_hs',
+      //   text: '78000 horas',
+      //   fill: d => (d.anio_mision == '2016' ? 'white' : 'transparent'),
+      //   fpntWeight: 'bold',
+      //   fontSize: 18,
+      //   dy: -5,
+      // }),
+      Plot.axisY(d3.ticks(0, 80000, 8)),
+      
       
     ],
-    y: {
-      grid: true
+    x: {
+      label: 'Anio',
+      labelOffset: 30
     },
+    y: {
+      label: 'Horas de mision',
+      grid: true,
+      labelOffset: 5,
+    },
+    style: {
+      fontFamily: 'sans-serif',
+      fontSize: 14,
+      //background: 'hsl(0, 1%, 50%)',
+      padding: '50px'
+    },
+    width: 900,
     height: 1000,
-    marginLeft: 100,
+    marginLeft: 80,
+    inset: 20
   })
  
   d3.select('#chart').append(() => chart)
