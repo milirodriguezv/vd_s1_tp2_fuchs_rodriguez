94e4ad44dd36d2a84a394ebaef4aeb1ef1b05dba
milirodriguezv <milagrosrodriguez1602@gmail.com>
2023-03-15 11:34:35 -0300

    dataviz4 nuevo

--- dataviz_4/script.js
+++ dataviz_4/script.js
@@ -3,43 +3,22 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
   console.log(data)
 
   let chart = Plot.plot({
-    line: true,
-    nice: true,
-    zero: true,
-    grid: true,
-    color: {
-      legend: true,
-      range: ['violet', 'orange'],
-    },
     marks: [
-      Plot.dot(
-        data.filter(d => d.nacionalidad == 'EE.UU.'),
-        {
-        x: 'mision_hs',
-        y: 'eva_mision_hs',
-        fill: 'genero'
+      Plot.barY(data, {
+        x: 'anio_mision',
+        y: 'mision_hs',
       }),
     ],
-    width: 640,
-    height: 400,
-    margin: 60,
-    inset: 80,
-    zero: true,
+    color: {
+      scheme: 'greens',
+      legend: true,
+    },
     x: {
-      label: 'Fertilidad',
-      labelOffset: 40,
+      grid: true,
     },
-    y: {
-      label: 'Grupo de países',
-      labelOffset: 50,
-    },
-    style: {
-      fontFamily: 'sans-serif',
-      fontSize: 13,
-      //background: 'hsl(0, 100%, 50%)',
-      color: 'black',
-      padding: '10px',
-    },
+    height: 1000,
+    marginLeft: 100,
   })
+ 
   d3.select('#chart').append(() => chart)
 })
