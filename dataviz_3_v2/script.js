961136e73f06c668294afe9efed2779989093755
valen fuchs <fuchsvalen@gmail.com>
2023-03-20 08:06:56 -0300

    h

--- dataviz_3_v2/script.js
+++ dataviz_3_v2/script.js
@@ -3,25 +3,42 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
   console.log(data)
 
   let chart = Plot.plot({
+    x: {domain: [2000, 2020]},
     marks: [
-      Plot.dot(data.filter(d => d.nacionalidad == 'U.S.S.R/Rusia' || d.nacionalidad =='EE.UU.'), {
+      Plot.dot(data, {
         x: "anio_mision",
         y: "mision_hs",
-        fill: "nacionalidad",
-        fillOpacity: 0.6,
+        fill: d => (d.nacionalidad == 'EE.UU.' ? '#E53935 ' : d.nacionalidad == 'U.S.S.R/Rusia' ? '#1976D2 ' : 'transparent'),
+        r: 5,
       }),
       Plot.frame(),
     ],
+    y: {
+      label: 'Horas de Mision',
+    },
+    x: {
+      domain: [2000, 2020], //no andaaaaa
+      label: 'Anio',
+      labelOffset: 30
+    },
     grid: true,
     nice: true,
     zero: true,
-    width: 800,
-    height: 200,
-    facet: {
-      data: data.filter(d => d.nacionalidad == 'U.S.S.R/Rusia' || d.nacionalidad =='EE.UU.'),
-      x: "nacionalidad",
-      label: 'Nacionalidad'
+    marginBottom: 60, 
+    marginLeft: 60, 
+    width: 500,
+    height: 300,
+    style:{
+      fontFamily: 'sans-serif',
+      fontSize: 12,
+      color:'black', 
+      padding: '10px',
     },
+    facet:{
+      data: data.filter(d => d.nacionalidad == 'U.S.S.R/Rusia' || d.nacionalidad == 'EE.UU.'),
+      //data: data,
+      x: "nacionalidad",
+    }
   });
     
   d3.select('#chart').append(() => chart)
