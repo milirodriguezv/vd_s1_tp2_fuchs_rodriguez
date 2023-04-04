dee017542145e4ee8396d8f8cbe2f35b23e95026
milirodriguezv <milagrosrodriguez1602@gmail.com>
2023-04-04 19:03:53 -0300

    commit final

---
+++ exploracion/data 2-viejo/script.js
@@ -0,0 +1,30 @@
+d3.csv('astronautas.csv', d3.autoType).then(data => {
+  
+  console.log(data)
+
+  let chart = Plot.plot({
+    marks: [
+      Plot.dot(data,
+        //data.filter(d => d.genero == 'femenino'),
+        {
+          x: 'edad_mision',
+          y: 'mision_hs',
+          z: 'genero',
+          symbol: 'genero'
+        },
+      ),
+    ],
+    symbol: {
+      legend: true,
+    },
+    x: {
+      // https://github.com/observablehq/plot#formats
+      tickFormat: 'd',
+    },
+    y: {
+      nice: true,
+      grid: true,
+    },
+  })
+  d3.select('#chart').append(() => chart)
+})
