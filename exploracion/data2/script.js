9e83284a8dacec4cd43f8158b39b6f2a092b45ab
milirodriguezv <milagrosrodriguez1602@gmail.com>
2023-04-04 18:59:09 -0300

    .

---
+++ exploracion/data2/script.js
@@ -0,0 +1,51 @@
+d3.csv('astronautas.csv', d3.autoType).then(data => {
+  
+  console.log(data)
+
+  let chart = Plot.plot({
+    marks: [
+      Plot.barY(data, Plot.groupX({y: 'sum'},
+        {x: 'anio_mision', y: 'mision_hs',
+        fill: d => (d.anio_mision == 2016 ? 'purple' :  'orange')},
+      )),
+     
+      // Plot.axisX({y: (x) => data.find((d) => d.mision_hs >= x)?.anio_mision,  //poner los anios arriba de cada barra
+      // fontFamily: 'Arial',
+      // fontWeight: 'bold', 
+      //   fontSize: 20,
+      //   tickSize: 0,
+      // }),
+  
+      // Plot.axisX({fill: "white", tickSize: 0}),
+      
+    ],
+    x: {
+      label:'Año de misión',
+      tickFormat: 'd',
+      labelOffset: 60,
+    },
+
+    y: {
+      label: 'Horas de misión',
+      ticks: 7,
+      domain: [0, 80000],
+      tickFormat: 'd',
+      grid: true,
+      labelOffset: 50,
+      insetTop: 50
+    },
+
+    style: {
+      fontFamily: 'Verdana',
+      fontSize: 14,
+      padding: '50px'
+    },
+    width: 900,
+    height: 1000,
+    marginLeft: 70,
+    marginBottom: 70,
+    inset: 20
+  })
+ 
+  d3.select('#chart').append(() => chart)
+})
